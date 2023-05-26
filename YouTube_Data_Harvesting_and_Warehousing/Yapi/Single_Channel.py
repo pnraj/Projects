8f1c4815967d9c71874cb34e85efe1a9bbe18751
Nataraj Palanivel <pnrajk@gmail.com>
2023-05-26 11:14:48 +0530

    Update Single_Channel.py

--- YouTube_Data_Harvesting_and_Warehousing/Yapi/Single_Channel.py
+++ YouTube_Data_Harvesting_and_Warehousing/Yapi/Single_Channel.py
@@ -4,12 +4,11 @@ import pandas as pd
 import streamlit as st
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-#API_KEY = 'AIzaSyCH53t5KoEdr9JCkjQao84lHrDPmpF1oR4'
-#youtube = build('youtube', 'v3', developerKey=API_KEY)
+
 
 #channel_id_extract(channel_link)
 def Channel_data(all_id):
-    API_KEY = 'AIzaSyAXoC29qwkPhZUralbjrdGNvBRXGRURKeU'
+    API_KEY = st.secrets['apikey1']
     youtube = build('youtube', 'v3', developerKey=API_KEY)
     
     cha_res = youtube.channels().list(
@@ -34,7 +33,7 @@ def Channel_data(all_id):
     return ch_ti1
 # video id 
 def get_video_ids(playlist_id):
-    API_KEY = 'AIzaSyAXoC29qwkPhZUralbjrdGNvBRXGRURKeU'
+    API_KEY = st.secrets['apikey2']
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     video_ids = []
@@ -64,7 +63,7 @@ def get_video_ids(playlist_id):
 
 ## new version of get_video_details
 def get_video_details(video_ids):
-    API_KEY = 'AIzaSyAXoC29qwkPhZUralbjrdGNvBRXGRURKeU'
+    API_KEY = st.secrets['apikey3']
     youtube = build('youtube', 'v3', developerKey=API_KEY)
     #video_ids = video_ids[:100]
     all_video_stats = []
@@ -96,7 +95,7 @@ def get_video_details(video_ids):
     return all_video_stats
 
 def comment_data(vid_lis):
-    API_KEY = 'AIzaSyAXoC29qwkPhZUralbjrdGNvBRXGRURKeU'
+    API_KEY = st.secrets['apikey4']
     youtube = build('youtube', 'v3', developerKey=API_KEY)
         
     comments = []
